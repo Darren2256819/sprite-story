381fb880bab8a10acccb1310f75199e3a87acbb4
Darren2256819 <82167088+Darren2256819@users.noreply.github.com>
2021-05-06 13:23:19 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+scene.setBackgroundColor(7)
 let normalMonkey = sprites.create(img`
     . . . . f f f f f . . . . . . . 
     . . . f e e e e e f . . . . . . 
@@ -17,26 +18,32 @@ let normalMonkey = sprites.create(img`
     . . . . f f f f f f f f f . . . 
     `, SpriteKind.Player)
 let happyMonkey = sprites.create(img`
-    . . . . . f f f f f . . . . . . 
-    . . . . f e e e e e f . . . . . 
-    . . . f d d d d d d e f . . . . 
-    . . f d f f d d f f d f f . . . 
-    . c d d d e e d d d d e d f . . 
-    . c d c d d d d c d d e f f . . 
-    . c d d c c c c d d d e f f f f 
-    . . c d d d d d d d e f f b d f 
-    . . . c d d d d e e f f f d d f 
-    . . . . f f f e e f e e e f f f 
-    . . . . f e e e e e e e f f f . 
-    . . . f e e e e e e f f f e f . 
-    . . f f e e e e f f f f f e f . 
-    . f b d f e e f b b f f f e f . 
-    . f d d f f f f d d b f f f f . 
-    . f f f f f f f f f f f f f . . 
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e f c . . . . 
+    . f d d d d e e e f f . . . . . 
+    . . f e e e f f e e e f . . . . 
+    . . f f f f f e e e e e f . f f 
+    . . f d b f e e f f e e f . e f 
+    . f f d d f e f f e e e f . e f 
+    . f f f f f f e b b f e f f e f 
+    . f d d f e e e d d b e f f f f 
+    . . f f f f f f f f f f f f f . 
     `, SpriteKind.Player)
 normalMonkey.setPosition(50, 50)
 happyMonkey.setPosition(120, 80)
 happyMonkey.say("Hi, I'm happy!", 1000)
+animation.runMovementAnimation(
+happyMonkey,
+animation.animationPresets(animation.bobbing),
+2000,
+true
+)
 pause(2000)
 happyMonkey.say("How are you doing?", 2000)
 pause(2000)
@@ -93,5 +100,11 @@ let happyMonkey2 = sprites.create(img`
     . f d d f e e f d d b f f f f . 
     . f f f f f f f f f f f f f . . 
     `, SpriteKind.Player)
+animation.runMovementAnimation(
+happyMonkey2,
+animation.animationPresets(animation.shake),
+2000,
+true
+)
 happyMonkey2.setPosition(50, 50)
 happyMonkey2.say(":DDD", 5000)
